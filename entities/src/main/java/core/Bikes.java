017ec277354c1a420579ccb952f9fe2dcca99870
Sebastjan Kozoglav <sk5429@student.uni-lj.si>
2018-11-24 18:11:23 +0100

    Get bikes by region

---
+++ entities/src/main/java/core/Bikes.java
@@ -0,0 +1,84 @@
+package core;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import java.io.Serializable;
+
+@Entity(name = "Bikes")
+@NamedQueries({
+        @NamedQuery(name = "Bikes.getAll", query = "SELECT b from Bikes b"),
+        @NamedQuery(name = "Bikes.getByRegion", query = "SELECT b FROM Bikes b INNER JOIN Users u ON u.user_id=b.user_id WHERE u.region = :region")
+})
+public class Bikes implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bike_id;
+    private Integer user_id;
+    private String type;
+    private String size;
+    private String description;
+    private double price;
+    private boolean isAvailable;
+
+    public Integer getBike_id() {
+        return bike_id;
+    }
+
+    public void setBike_id(Integer bike_id) {
+        this.bike_id = bike_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @ManyToOne
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+}
---
+++ services/src/main/java/beans/BikesBean.java
@@ -0,0 +1,38 @@
+package beans;
+
+import core.Bikes;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@ApplicationScoped
+public class BikesBean {
+
+    @PersistenceContext(unitName = "cityBikeShare-jpa")
+    private EntityManager entityManager;
+
+    public List<Bikes> getBikes() {
+        TypedQuery<Bikes> query = entityManager.createNamedQuery("Bikes.getAll", Bikes.class);
+        return query.getResultList();
+    }
+
+    public Bikes getBikeById(int bikeId) {
+        return entityManager.find(Bikes.class, bikeId);
+    }
+
+    public List<Bikes> getBikesByRegion(String region) {
+        TypedQuery<Bikes> query = entityManager.createNamedQuery("Bikes.getByRegion", Bikes.class);
+        query.setParameter("region", region);
+
+        try {
+            return query.getResultList();
+        } catch (NoResultException | NonUniqueResultException e) {
+            return null;
+        }
+    }
+}
