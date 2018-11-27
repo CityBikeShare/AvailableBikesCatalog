5d3bf6323e2fdf378faf21921ab63cabe43e2f5d
Sebastjan Kozoglav <sk5429@student.uni-lj.si>
2018-11-27 14:24:12 +0100

    Config file update

---
+++ api/src/main/java/rest/sources/InfoSource.java
@@ -0,0 +1,75 @@
+package rest.sources;
+
+
+import core.Bikes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("info")
+@ApplicationScoped
+public class InfoSource {
+
+    @Operation(
+            description = "Get project information",
+            tags = "info",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Info received successfully",
+                            content = @Content(schema = @Schema(implementation = Bikes.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Info failed to receive",
+                            content = @Content(schema = @Schema(implementation = Error.class))
+                    )
+            }
+    )
+    @GET
+    public Response getInfo() {
+        JsonObject json = Json.createObjectBuilder()
+                .add("clani", Json.createArrayBuilder().add("sk5429"))
+                .add("opis_projekta", "Moj projekt implementira aplikacijo za deljenje koles.")
+                .add("mikrostoritve", Json.createArrayBuilder()
+                                        .add("/sources/bikes")
+                                        .add("/sources/bikerent")
+                                        .add("/sources/users"))
+                .add("github", Json.createArrayBuilder()
+                                        .add("https://github.com/CityBikeShare/BikeRentService")
+                                        .add("https://github.com/CityBikeShare/BikeCatalogService")
+                                        .add("https://github.com/CityBikeShare/UserManagmentService")
+                                        .add("https://github.com/CityBikeShare/PayingService")
+                                        .add("https://github.com/CityBikeShare/Documentation"))
+                .add("travis", Json.createArrayBuilder()
+                                        .add("https://travis-ci.org/CityBikeShare/BikeRentService")
+                                        .add("https://travis-ci.org/CityBikeShare/BikeCatalogService")
+                                        .add("https://travis-ci.org/CityBikeShare/UserManagmentService")
+                                        .add("https://travis-ci.org/CityBikeShare/PayingService"))
+                .add("dockerhub", Json.createArrayBuilder()
+                                        .add("https://hub.docker.com/r/citybikeshare/bikerentservice/")
+                                        .add("https://hub.docker.com/r/citybikeshare/bikecatalogservice/")
+                                        .add("https://hub.docker.com/r/citybikeshare/usermanagmentservice/")
+                                        .add("https://hub.docker.com/r/citybikeshare/payingservice/"))
+                .build();
+
+
+        return Response.ok(json.toString()).build();
+
+    }
+
+}
